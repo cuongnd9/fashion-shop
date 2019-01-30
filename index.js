11711c1800dd8ee1474d089d4b6ea09689cabe8a
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-01-30 15:53:36 +0700

    🚀 home route

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 require('dotenv').config()
 
 const express = require('express')
+const pug = require('pug')
 
-const port = 3000
+const port = 8000
 
 app = express()
 
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-	res.send('Home')
+	res.render('index')
 })
 
 app.listen(port, () => console.log(`App is listening on ${port}`))
